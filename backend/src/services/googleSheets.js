7093fce5df9aa39a0f81027c983c55d12177d5e4
Sashvatha20 <sashvatha20@gmail.com>
2026-06-24 12:46:32 +0530

    Updated the googlesheets.js file

--- backend/src/services/googleSheets.js
+++ backend/src/services/googleSheets.js
@@ -1,14 +1,18 @@
-const path = require('path');
 const { google } = require('googleapis');
-
 const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
 
 if (!spreadsheetId) {
   throw new Error('GOOGLE_SPREADSHEET_ID is missing in environment variables');
 }
 
+if (!process.env.GOOGLE_SERVICE_ACCOUNT_JSON) {
+  throw new Error('GOOGLE_SERVICE_ACCOUNT_JSON is missing in environment variables');
+}
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON);
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.resolve(process.cwd(), process.env.GOOGLE_SERVICE_ACCOUNT_KEY_FILE || './service-account.json'),
+  credentials: serviceAccount,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
